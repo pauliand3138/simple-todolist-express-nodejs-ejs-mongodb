e66840c9ad1743eefec3ee2cfe8d82936a864925
Paul <paul.iand3138@hotmail.com>
2022-05-15 16:15:41 +0800

    edited styles

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const date = require(__dirname + "/date.js");
 
 const app = express();
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["House chores", "Buy Groceries", "Do Exercises"];
 const workItems = [];
 
 app.set("view engine", "ejs");
